5eaa0494058646b9a0b3a5963ff263b4a005cbc6
Siddhesh-Bhavar <73816605+Siddhesh-Bhavar@users.noreply.github.com>
2021-04-18 16:02:29 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -2,16 +2,16 @@ class Bob {
   constructor(x, y,r) {
     var options = {
         isStatic:false,
-        'restitution':0.3,
-        'friction':0.5,
-        'density':1.2
+        'restitution':1,
+        'friction':0,
+        'density':0.8
     }
 
     this.x=x;
     this.y=y;
     this.r=r
     
-    this.body=Bodies.circle(this.x/2, this.y/2, this.r/2, options)
+    this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
     World.add(world, this.body);
   }
   display(){
@@ -21,7 +21,6 @@ class Bob {
     push();
     translate(bob.x, bob.y);
     rectMode(CENTER);
-    strokeWeight(3);
     fill(255,0,255);
     ellipse(0, 0,this.r, this.r);
     pop();
